43434462c0ca256d0fff398fbbd759ac7e1b2134
shubhro gupta <shubhrogupta117@gmail.com>
2019-08-27 15:32:32 +0530

    asdasd

--- twitter/src/Tweet.js
+++ twitter/src/Tweet.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 
-function Tweet(props){
+function Tweet({name, message}){
     return(
         <div className="tweet">
-            <h3>{props.name}</h3>
-            <p>{props.message}</p>
+            <h3>{name}</h3>
+            <p>{message}</p>
             <h3>number of likes</h3>
         </div>
     );
